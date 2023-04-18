bb9be6c70773d923d167e7aa562f3fcf97613ef5
Nathan Schoedl <nathanschoedl@ufl.edu>
2023-04-18 15:40:04 -0400

    Authenticate file without clientID/Secret

---
+++ src/server/api/authenticate.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"context"
+	"log"
+
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func Authenticate() *spotify.Client {
+
+	authConfig := &clientcredentials.Config{
+		ClientID:     "enter clientID here",
+		ClientSecret: "enter clientSecret here",
+		TokenURL:     spotify.TokenURL,
+	}
+
+	accessToken, err := authConfig.Token(context.Background())
+	if err != nil {
+		log.Fatalf("error retrieve access token: %v", err)
+	}
+
+	a := spotify.NewAuthenticator("/response", spotify.ScopePlaylistModifyPublic)
+	url := a.AuthURL(authConfig.ClientID)
+	log.Println("Please log in:", url)
+	client := a.NewClient(accessToken)
+
+	return &client
+}
